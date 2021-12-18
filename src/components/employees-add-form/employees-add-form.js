1c35842d7f075a8da5a410d795dbc8d981be6f94
Doom <vnagorniyrnd@gmail.com>
2021-12-18 17:18:45 +0300

    знакомство и добавление управляемого компонента

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,27 +1,50 @@
+import { Component } from 'react';
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-  return (
-    <div className='app-add-form'>
-      <h3>Добавьте нового сотрудника</h3>
-      <form className='add-form d-flex'>
-        <input
-          type='text'
-          className='form-control new-post-label'
-          placeholder='Как его зовут?'
-        />
-        <input
-          type='number'
-          className='form-control new-post-label'
-          placeholder='З/П в $?'
-        />
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      salary: '',
+    };
+  }
+  onValueChanges = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-        <button type='submit' className='btn btn-outline-light'>
-          Добавить
-        </button>
-      </form>
-    </div>
-  );
-};
+  render() {
+    const { name, salary } = this.state;
+    return (
+      <div className='app-add-form'>
+        <h3>Добавьте нового сотрудника</h3>
+        <form className='add-form d-flex'>
+          <input
+            type='text'
+            name='name'
+            value={name}
+            className='form-control new-post-label'
+            placeholder='Как его зовут?'
+            onChange={this.onValueChanges}
+          />
+          <input
+            type='number'
+            name='salary'
+            value={salary}
+            className='form-control new-post-label'
+            placeholder='З/П в $?'
+            onChange={this.onValueChanges}
+          />
+
+          <button type='submit' className='btn btn-outline-light'>
+            Добавить
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default EmployeesAddForm;
